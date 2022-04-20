ac361a47de1ce18d1eb5f243d2a1ad239ae16257
Carlos Dev <80906720+CarlosDev101@users.noreply.github.com>
2022-04-19 22:05:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import decouple, os
 from discord.ext import commands
 
 bot = commands.Bot("!")
+bot.remove_command("help")
+
 
 def load_cogs(bot):
 
@@ -9,11 +11,17 @@ def load_cogs(bot):
         if comandos.endswith(".py"):
             cog2 = comandos[:-3]
             bot.load_extension(f"Commands.{cog2}")
-            
+
     for commands in os.listdir("Commands/Administracao"):
         if commands.endswith(".py"):
             cog3 = commands[:-3]
             bot.load_extension(f"Commands.Administracao.{cog3}")
+    
+    
+    for commands in os.listdir("Commands/Leveling"):
+        if commands.endswith(".py"):
+            cog3 = commands[:-3]
+            bot.load_extension(f"Commands.Leveling.{cog3}")
 
 load_cogs(bot)            
 
