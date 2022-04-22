855d04c68c8a058ae020423b6bd50ad04276705a
Carlos Dev <80906720+CarlosDev101@users.noreply.github.com>
2022-04-22 00:25:42 -0300

    Update Help.py

--- Commands/Administracao/Help.py
+++ Commands/Administracao/Help.py
@@ -8,44 +8,33 @@ class Help(commands.Cog):
     @commands.command(name="Help", alisases=['ajuda', "Ajuda"], help="Comando onde contem os comandos do bot")
     async def Help(self, ctx):
         embed = discord.Embed(
-                    title=f'Comandos da Mitsuy [pág 1/3]',
-                    colour= 0x0000ff,
-                    description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy. 
-                    \n**Avatar** - Vizualizar a foto de perfil do membro
-                    \n**Clear** - Apaga as mensagens do servidor
-                    \n**Createchannel** - Cria um canal de texto
-                    \n**Delchannel** - Deletar canal de texto
-                    \n**Help** - Ver os comandos do bot
-                    \n**Loock** - Tranca o canal de texto
-                    \n**Ping** - Ver a latencia
-                    \n**Say** - Manda uma mensagem de texto através do bot
-                    \n**Seeunlock** - Permitir que os membros vejam o canal
-                    \n**Seelock** - Impedir que os membros comuns vejam o canal
-                    \n**Traduzir** - Traduz uma mensagem para outro idioma
-                    \n**Delay** - Coloca um delay no canal de texto
-                    \n**Ban** - Vai banir um membro
-                    \n**Unban** - Vai desbanir um membro
-                    \n**Kick** - Expulsa um membro do servidor
-                    \n**Mute** - Silencia um membro
-                     \n**Unmute** - Tira o membro do mute
+                    title='Menu de ajuda',
+                    description=f"""Olá {ctx.author.mention}, reaja algum dos emojis abaixo para você ver a lista de comandos!
+                    \nVocê vai receber na sua DM a lista de comandos. Para isso, certifique-se se sua DM esteja aberta.
+
+                    👮🏼‍♂️  **Administrção**
+                    🎮 **Jogos**
+                    🎧 **Música**
+
                     \n[Instagram do dono](https://www.instagram.com/carlosdev10/)
                     \n[Servidor para suporte](https://discord.gg/9TcEt3bF)
                     """
+
         )
         embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
         resposta = await ctx.reply(embed=embed, delete_after=60)
-        await resposta.add_reaction("1️⃣")
-        await resposta.add_reaction("2️⃣")
-        await resposta.add_reaction("3️⃣")
+        await resposta.add_reaction("👮🏼‍♂️")
+        await resposta.add_reaction("🎮")
+        await resposta.add_reaction("🎧")
     
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, message):
         while True:
             if reaction.emoji == self.bot.user:
                 pass
-            if reaction.emoji == "1️⃣":
+            if reaction.emoji == "👮🏼‍♂️":
                 embed = discord.Embed(
-                        title=f'Comandos da Mitsuy [pág 1/3]',
+                        title=f'Comandos da Mitsuy [pág 1/2]',
                         colour= 0x0000ff,
                         description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy. 
                         \n**Avatar** - Vizualizar a foto de perfil do membro
@@ -71,14 +60,51 @@ class Help(commands.Cog):
                 )
                 embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
                 resposta = await message.send(embed=embed, delete_after=60)
-                await resposta.add_reaction("1️⃣")
-                await resposta.add_reaction("2️⃣")
-                await resposta.add_reaction("3️⃣")
-                break
+                await resposta.add_reaction("⏩")
 
-            elif reaction.emoji == "2️⃣":
+                
+                if reaction.emoji == "⏩":
+                    embed = discord.Embed(
+                        title=f'Comandos da Mitsuy [pág 2/2]',
+                        colour= 0x0000ff,
+                        description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy. 
+                        \n**Seelock** - Impedir que os membros comuns vejam o canal
+                        \n**Traduzir** - Traduz uma mensagem para outro idioma
+                        \n**Delay** - Coloca um delay no canal de texto
+                        \n**Ban** - Vai banir um membro
+                        \n**Unban** - Vai desbanir um membro
+                        \n**Kick** - Expulsa um membro do servidor
+                        \n**Mute** - Silencia um membro
+                        \n**Unmute** - Tira o membro do mute
+                        \n[Instagram do dono](https://www.instagram.com/carlosdev10/)
+                        \n[Servidor para suporte](https://discord.gg/9TcEt3bF)
+                        """
+                    )
+                    embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
+                    resposta = await message.send(embed=embed, delete_after=60)
+                    break
+
+
+
+            elif reaction.emoji == "🎮":
                 embed = discord.Embed(
-                        title=f'Comandos da Mitsuy [pág 2/3]',
+                        title=f'Comandos da Mitsuy',
+                        colour= 0x0000ff,
+                        description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy.
+                        \n**Tictoe** - Jogue o jogo da velha (Necessário dois jogadores)
+                        \n[Instagram do dono](https://www.instagram.com/carlosdev10/)
+                        \n[Servidor para suporte](https://discord.gg/9TcEt3bF)
+                        """
+                )
+                embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
+                resposta = await message.send(embed=embed, delete_after=60)
+
+                break
+            
+
+            elif reaction.emoji == "🎧":
+                embed = discord.Embed(
+                        title=f'Comandos da Mitsuy',
                         colour= 0x0000ff,
                         description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy.
                         \n**Play** - Toca uma música
@@ -91,26 +117,6 @@ class Help(commands.Cog):
                 )
                 embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
                 resposta = await message.send(embed=embed, delete_after=60)
-                await resposta.add_reaction("1️⃣")
-                await resposta.add_reaction("2️⃣")
-                await resposta.add_reaction("3️⃣")
-                break
-
-            elif reaction.emoji == "3️⃣":
-                embed = discord.Embed(
-                        title=f'Comandos da Mitsuy [pág 2/3]',
-                        colour= 0x0000ff,
-                        description ="""Olá, muito obrigado por estar utilizando o bot Mitsuy.
-                        \n**Tictoe** - Jogue o jogo da velha (Necessário dois jogadores)
-                        \n[Instagram do dono](https://www.instagram.com/carlosdev10/)
-                        \n[Servidor para suporte](https://discord.gg/9TcEt3bF)
-                        """
-                )
-                embed.set_author(name= self.bot.user.name, icon_url = self.bot.user.avatar_url)
-                resposta = await message.send(embed=embed, delete_after=60)
-                await resposta.add_reaction("1️⃣")
-                await resposta.add_reaction("2️⃣")
-                await resposta.add_reaction("3️⃣")
                 break
             
 
